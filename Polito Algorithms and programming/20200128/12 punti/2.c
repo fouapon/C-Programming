db35d74e76419db6d87c7a3982aa845d4bd769f0
fouapon <fouapon@gmail.com>
2020-02-05 09:59:31 +0100

    Update 2.c

--- Polito Algorithms and programming/20200128/12 punti/2.c
+++ Polito Algorithms and programming/20200128/12 punti/2.c
@@ -17,7 +17,20 @@ link NEW(int item, link left, link right){
 	x->item=item; x->left=left;x->right=right;
 	return x;
 }
-
+link buildTreeR(int *inorder, int *preorder,int *pos, int a , int b ){
+    int i, p;
+    if(a>=b) return NULL;
+    p=(*pos)++;
+    for(i=0;i<b;i++){
+        if(inorder[i]==preorder[p]){
+            break;
+        }
+    }
+    link left = buildTreeR(inorder,preorder,pos, a, i);
+    link right = buildTreeR(inorder,preorder,pos, i+1, b);
+	return NEW(preorder[p], left, right);;
+}
+/*
 link buildTreeR(int *inorder, int *preorder,int *pos, int a , int b ){
     int i;
     if(a>=b) return NULL;
@@ -31,6 +44,7 @@ link buildTreeR(int *inorder, int *preorder,int *pos, int a , int b ){
     root->right = buildTreeR(inorder,preorder,pos, i+1, b);
 	return root;
 }
+*/
 link buildTree(int *inorder, int *preorder, int N){
     int pos = 0;
     link root = buildTreeR(inorder,preorder, &pos, 0, N);
@@ -46,22 +60,35 @@ link buildTree(int *inorder, int *preorder, int N){
 /*
 codice di prova
 */
-void print_tree(link root, int pos){
+void print_treeR(link node, int pos, char* str){
     int i;
-    if(root==NULL) return;
+    if(node==NULL) return;
     for(i=0;i<pos;i++){
-        printf("-");
+        printf("--");
     }
-    printf("%d\n",root->item);
-    print_tree(root->left,pos+1);
-    print_tree(root->right,pos+1);
+    printf(">%s: %d\n",str,node->item);
+    print_treeR(node->left,pos+1,"left_");
+    print_treeR(node->right,pos+1,"right");
     return;
 }
+void print_tree(link root){
+    int i;
+    if(root==NULL) return;
+    printf("root: %d\n",root->item);
+    print_treeR(root->left,1," left_");
+    print_treeR(root->right,1,"right");
+    return;
+}
+
 int main(){
-    int inorder[]={4,2,5,1,3};
-    int preorder[]={1,2,4,5,3};
-    int N=5;
-    link root = buildTree(inorder,preorder,N);
-    print_tree(root,0);
+
+    int inorder1[]={4,2,5,1,3};
+    int preorder1[]={1,2,4,5,3};
+    int N1=5;
+
+    int inorder[]={2,7,4,1,5,3,6};
+    int preorder[]={1,2,4,7,3,5,6};
+    int N=7;
+    print_tree(buildTree(inorder,preorder,N));
     return 0;
 }
