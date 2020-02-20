778507cd4cca57f904197675b176f4934e5267aa
fouapon <fouapon@gmail.com>
2020-02-20 18:52:46 +0100

    Update 1.c

--- Polito Algorithms and programming/20160216/12_punti/1.c
+++ Polito Algorithms and programming/20160216/12_punti/1.c
@@ -7,14 +7,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+Soluzione
+*/
 int areaTot(FILE *fp){
     int MAX_X = 10, MAX_Y = 10;
 	int i, j, x1, y1, x2, y2;
 	int area=0;
-	int **mat=malloc(MAX_X*sizeof(int*));
+	int **mat;
+	
+	//allocare e inizializzare a zero una matrice mat[MAX_X][MAX_Y]
+	mat=malloc(MAX_X*sizeof(int*));
 	for(i=0;i<MAX_X;i++)
 		mat[i]=calloc(MAX_Y,sizeof(int));
-
 
 	while(fscanf(fp,"%d%d%d%d",&x1,&y1,&x2,&y2)==4){
         //printf("%d %d %d %d\n",x1,y1,x2,y2);
@@ -29,6 +34,10 @@ int areaTot(FILE *fp){
 }
 
 
+
+/*
+codice di prova
+*/
 int main (){
     printf("%d\n",areaTot(fopen("in.txt","r")));
 }
